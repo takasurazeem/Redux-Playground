c5a35ab87ca7280ab1c723dad5ae5a5218e5c6f8
Takasur Azeem <takasurazeem@gmail.com>
2024-05-05 02:15:07 +0500

    Initial Commit

---
+++ Redux Playground/Redux_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redux_PlaygroundApp.swift
+//  Redux Playground
+//
+//  Created by Takasur Azeem on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Redux_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

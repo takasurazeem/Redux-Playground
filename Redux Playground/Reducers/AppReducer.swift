742c2fa827335c6caccc74e83844e77b0167981f
Takasur Azeem <takasurazeem@gmail.com>
2024-05-13 02:39:15 +0500

    Assign counter state its state

--- Redux Playground/Reducers/AppReducer.swift
+++ Redux Playground/Reducers/AppReducer.swift
@@ -12,7 +12,8 @@ func appReducer(
   _ state: AppState,
   _ action: Action
 ) -> AppState {
-  
+  var state = state
+  state.counterState = counterReducer(state.counterState, action)
   return state
 }
 
